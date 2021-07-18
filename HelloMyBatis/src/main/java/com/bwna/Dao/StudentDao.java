90abfb22017435139e31b5e4440d6f8e7b41185e
bwna <1478432593@qq.com>
2021-07-18 23:33:51 +0800

    最后的提交

--- HelloMyBatis/src/main/java/com/bwna/Dao/StudentDao.java
+++ HelloMyBatis/src/main/java/com/bwna/Dao/StudentDao.java
@@ -1,7 +1,9 @@
 package com.bwna.Dao;
 
 import com.bwna.domian.Student;
+import org.apache.ibatis.annotations.Param;
 
+import java.util.Iterator;
 import java.util.List;
 
 public interface StudentDao {
@@ -14,4 +16,10 @@ public interface StudentDao {
 
     //插入学生信息
     int insertStudent(Student student);
+
+    //根据多参数查询学生信息
+    List<Student> selectStudentByMultiParam(@Param("id") Integer id,
+                                             @Param("name") String name);
+    //用student对象来查询学生信息
+    List<Student> selectStudentByStudent(Student student);
 }
--- HelloMyBatis/src/test/java/com/bwna/ParameterTypeTest.java
+++ HelloMyBatis/src/test/java/com/bwna/ParameterTypeTest.java
@@ -6,7 +6,33 @@ import com.bwna.utils.MyBatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.List;
+
 public class ParameterTypeTest {
+
+    //用student对象传入多个参数
+    @Test
+    public void TestBuStudent(){
+        SqlSession sqlSession = MyBatisUtils.getSqlSession();
+        StudentDao dao = sqlSession.getMapper(StudentDao.class);
+        Student student = new Student(1,"唐三","1111@qq.com",18);
+        List<Student> studentList = dao.selectStudentByStudent(student);
+        for(Student stu : studentList){
+            System.out.println("学生是：" + stu);
+        }
+    }
+
+    //传入多个参数
+    @Test
+    public void TestMultiparam(){
+        SqlSession sqlSession = MyBatisUtils.getSqlSession();
+        StudentDao dao = sqlSession.getMapper(StudentDao.class);
+        List<Student> student = dao.selectStudentByMultiParam(1002, "张三");
+        for(Student stu : student){
+            System.out.println("学生是：" + stu);
+        }
+    }
+
     @Test
     public void parameterTypeTest(){
         SqlSession sqlSession = MyBatisUtils.getSqlSession();
